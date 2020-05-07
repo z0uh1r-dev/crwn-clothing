3426c6a6a4b51cc6f94e6c4981f255c41e4db94b
z0uh1r-dev <zouhir.amahzoune@gmail.com>
2020-05-07 21:28:33 +0000

    Updating our app with shop page, collection preview component and collection item component

---
+++ src/components/collection/collection.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./collection.styles.scss";
+
+import CollectionItem from "../collection-item/collection-item.component.jsx";
+
+const Collection = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        .filter((item, index) => index < 4)
+        .map(({ id, ...item }) => (
+          <CollectionItem
+            key={id}
+            {...item}/>
+        ))}
+    </div>
+  </div>
+)
+
+export default Collection;
---
+++ src/pages/shoppage/shoppage.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import SHOP_DATA from './shop.data';
+
+import Collection from '../../components/collection/collection.component.jsx';
+
+class ShopPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA
+    }
+  }
+
+
+  render() {
+    const { collections } = this.state;
+    
+    return (<div className="shop-page">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <Collection key={id} {...otherCollectionProps} />
+      ))}
+    </div>)
+  }
+}
+
+export default ShopPage
