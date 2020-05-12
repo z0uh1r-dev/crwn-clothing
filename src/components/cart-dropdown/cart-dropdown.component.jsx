6a203b8fc8e5123f358500c876688d3e726f558b
z0uh1r-dev <zouhir.amahzoune@gmail.com>
2020-05-12 02:24:01 +0000

    updating our application with reselect, replaced cart selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,6 +6,8 @@ import './cart-dropdown.styles.scss';
 import CustomButton from '../custom-button/custom-button.component.jsx';
 import CartItem from '../cart-item/cart-item.component.jsx';
 
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+
 const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
@@ -17,8 +19,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 )
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(CartDropdown);
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/images/original.svg';
 
 import './cart-icon.styles.scss';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
-import { connect } from 'react-redux';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
-
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon"/>
-    <span className="item-count">0</span>
+    <span className="item-count">{ itemCount }</span>
   </div>
 )
 
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
-})
+});
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
