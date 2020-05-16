53c28dee4c164daf48203176f1dd6c2f3e8f2a48
z0uh1r-dev <zouhir.amahzoune@gmail.com>
2020-05-16 00:58:45 +0000

    removing redux-logger from production build, fixing shop categories links

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,12 @@ import { persistStore } from 'redux-persist';
 import rootReducer from './root-reducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const middlewares = [logger];
+
+const middlewares = [];
+
+if(process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 export const store = createStore(
   rootReducer, 
